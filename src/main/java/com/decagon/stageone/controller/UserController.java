8a7ddf3ef6106a29ac61af94687c744a7069049c
Olugbenga Falodun <olugbenga.falodun@etranzactng.com>
2020-06-02 12:27:07 +0100

    ... author API endpoint consumptin ...

---
+++ src/main/java/com/decagon/stageone/controller/UserController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.decagon.stageone.controller;
+
+import java.util.List;
+import com.decagon.stageone.service.UserService;
+
+/**
+ *
+ * @author Olugbenga.Falodun
+ */
+public class UserController {
+
+    private static UserService getUserService() {
+        return new UserService();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Size :: " + getUsernames(500).size());
+        
+        System.out.println("Author Username :: " + getUsernameWithHighestCommentCount());
+        
+        System.out.println("Author Username :: " + getUsernamesSortedByRecordDate(1333104319));
+    }
+
+    public static List<String> getUsernames(int threshold) {
+        UserService userService = getUserService();
+        return userService.getUsernames(threshold);
+    }
+
+    public static String getUsernameWithHighestCommentCount() {
+        UserService userService = getUserService();
+        return userService.getUsernameWithHighestCommentCount();
+    }
+
+    public static List<String> getUsernamesSortedByRecordDate(int threshold) {
+        UserService userService = getUserService();
+        return userService.getUsernamesSortedByRecordDate(threshold);
+    }
+}
